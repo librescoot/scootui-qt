644b5d040f62a729fb9c19a06c151b5f1a6ce608
Teal Bauer <teal@starsong.eu>
2026-03-16 16:52:24 +0100

    Revert "fix: desktop map rendering and GPS waiting state"
    This reverts commit c15453779354bb9523aeceb7c8b36e91f3525358.

--- src/services/MapService.h
+++ src/services/MapService.h
@@ -88,7 +88,6 @@ private:
     // Style
     void rebuildStyleUrl();
     QString rewriteStyleForMbtiles(const QString &qrcPath, const QString &mbtilesPath);
-    QString extractStyleToFile(const QString &qrcPath);
 
     // Route GeoJSON for native MapLibre layer
     void updateRouteGeoJson();
